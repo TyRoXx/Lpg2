91083eac51f646c7b9a974509b798f940347f96e
TyRoXx <tyroxxxx@gmail.com>
2023-12-13 14:03:39 +0100

    Trying to get coverage to work again

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 BUILD_DIR=../build-Lpg2
-./install_dependencies.sh $BUILD_DIR || exit 1
+./install_dependencies.sh || exit 1
 ./vcpkg/downloads/tools/cmake-3.27.1-linux/cmake-3.27.1-linux-x86_64/bin/cmake -B $BUILD_DIR -S . -G "CodeBlocks - Ninja" || exit 1
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-
-if [[ "$#" != 1 ]]; then
-    echo "Please provide enough arguments"
-    echo "Usage: $0 <build directory>"
-    exit 1
-fi
-
-BUILD_SUPER_DIR=$1
+BUILD_SUPER_DIR=`pwd`/..
 DEPENDENCIES_DIR=$BUILD_SUPER_DIR/lpg2_dependencies
 
-if apt; then
-    sudo apt install ninja-build g++-8 clang-format-3.9 || exit 1
-elif pacman; then
+if [ -f /usr/local/bin/apt ]; then
+    sudo apt install ninja-build g++ clang-format || exit 1
+elif [ -f pacman ]; then
     sudo pacman -Syy curl zip unzip tar --no-confirm || exit 1
 else
     echo "No supported package manager found"
@@ -20,11 +13,12 @@ fi
 mkdir -p $DEPENDENCIES_DIR || exit 1
 pushd $DEPENDENCIES_DIR || exit 1
 
-INSTALL_PREFIX=`pwd`/install.temp
+INSTALL_PREFIX=`pwd`/install
 rm -rf $INSTALL_PREFIX
 
 # lcov
-LCOV_VERSION=1.14
+sudo perl -MCPAN -e 'install "DateTime"' || exit 1
+LCOV_VERSION=2.0
 if [[ ! -d lcov-$LCOV_VERSION ]]; then
     wget https://github.com/linux-test-project/lcov/releases/download/v$LCOV_VERSION/lcov-$LCOV_VERSION.tar.gz || exit 1
     tar zxvf lcov-$LCOV_VERSION.tar.gz || exit 1
@@ -32,9 +26,6 @@ fi
 pushd lcov-$LCOV_VERSION || exit 1
 make -j4 PREFIX=$INSTALL_PREFIX install || exit 1
 popd || exit 1
-
-rm -rf install
-mv install.temp install || exit 1
 
 popd || exit 1
 
